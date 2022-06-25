0e3d0fd5ea79614960fdeec41a98e68709cb4911
Fabrizio <fabrisabella25@gmail.com>
2022-06-25 12:47:13 -0300

    Estilos basicos al home

--- lista/views.py
+++ lista/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+
+# Proveedores
+
+def muebles(request):
+    return render(request, 'proveedores/muebles.html')
+
+def articulos(request):
+    return render(request, 'proveedores/articulos.html')
+
+def madera(request):
+    return render(request, 'proveedores/madera.html')
--- lista_precios/urls.py
+++ lista_precios/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from lista import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home', views.home),
+    path('muebles', views.muebles, name='muebles'),
+    path('articulos', views.articulos, name='articulos'),
+    path('madera', views.madera, name='madera'),
 ]
